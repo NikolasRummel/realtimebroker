a2f1ea3bceb52bf54d104e753c0ed4fe1f4b5614
Nikolas Rummel <nikolasrummel6@gmail.com>
2024-12-26 03:15:48 +0100

    added basic broker and test client

---
+++ examples/node/client.ts
@@ -0,0 +1,38 @@
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+
+const PROTO_PATH = '../../protos/pubsub.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const proto = grpc.loadPackageDefinition(packageDefinition) as any;
+
+const client = new proto.pubsub.PubSubService('localhost:50051', grpc.credentials.createInsecure());
+
+const publishMessage = (topic: string, message: string) => {
+    client.Publish({ topic, message }, (error: any, response: any) => {
+        if (error) {
+            console.error(`Error publishing message: ${error.message}`);
+        } else {
+            console.log(`Published message to topic ${topic}: ${message}`);
+        }
+    });
+};
+
+const subscribeToTopic = (topic: string) => {
+    const call = client.Subscribe({ topic });
+
+    call.on('data', (message: any) => {
+        console.log(`Received message on topic ${message.topic}: ${message.message}`);
+    });
+
+    call.on('end', () => {
+        console.log(`Subscription to topic ${topic} ended.`);
+    });
+};
+
+subscribeToTopic('MAIN');
+
+setTimeout(() => {
+    publishMessage('MAIN', 'New user registered!');
+    publishMessage('INFO', 'Server is shutting down in 10 minutes.');
+}, 1000);
