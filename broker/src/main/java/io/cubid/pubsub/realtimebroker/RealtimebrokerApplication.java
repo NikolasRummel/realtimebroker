7b9c0c1e8a40d8a45fe92e24fdd5430e0a77cab3
Nikolas Rummel <nikolasrummel6@gmail.com>
2024-12-26 02:29:32 +0100

    first commit

---
+++ broker/src/main/java/io/cubid/pubsub/realtimebroker/RealtimebrokerApplication.java
@@ -0,0 +1,13 @@
+package io.cubid.pubsub.realtimebroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimebrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimebrokerApplication.class, args);
+	}
+
+}
---
+++ broker/src/test/java/io/cubid/pubsub/realtimebroker/RealtimebrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cubid.pubsub.realtimebroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimebrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
